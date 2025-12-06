ad654711c66edeb4f93670bf9db98310aaa773cf
lyongaelvira.ux <lyongaelvira@gmail.com>
2025-12-06 04:51:54 -0800

    Added index.html, script.js, style.css and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Learn-Link
 Student/Lecturer companion
+## Introduction
+Learn-Link is an educational support platform designed to help students learn. It supports collaborative learning and also constantly keeps the students updated on their academic life.
+To the lecturer, it reduces the bulk of papers on is desk because students submit assignments online and he also sends the feedback online.
+Equally, to the administration, Learn-Link is going to help them track both the students and lecturers progress. 
---
+++ script.js
@@ -0,0 +1,29 @@
+ocument.getElementById("registerForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let name= document.getElementById("name").ariaValueMax.trim();
+    let email= document. getElementById("email").ariaValueMax.trim();
+    let password= document. getElementById("password").ariaValueMax.trim();
+    let confirmPassword= document. getElementById("confirmPassword").ariaValueMax.trim();
+    let error= document. getElementById("errorMessage").ariaValueMax.trim();
+    let success= document. getElementById("successMessage").ariaValueMax.trim();
+
+    error.textContent="";
+    success.textContent="";
+
+    if(!name|| !email || !password || !confirmPassword){
+        error.textContent= "Please fill in all fields.";
+        return;
+    }
+    if(!email.includes("@")|| !email.includes(".")){
+        error.textContent= "Enter a valid email address.";
+        return;
+    }
+    if (password !== confirmPassword){
+        error.textContent= "Passwords do not match.";
+        return
+    }
+    success.textContent='Welcome to LearnLink, ${name}!' ;
+
+
+
+});
